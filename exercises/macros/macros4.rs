650912569b2ec1fa9e0d85044604dcbabc228342
lxsong <xuesongli@deepglint.com>
2023-10-19 14:42:49 +0800

    macros

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -3,19 +3,18 @@
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[rustfmt::skip]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
+    };
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
-    }
+    };
 }
 
 fn main() {
+    // vec![]
     my_macro!();
     my_macro!(7777);
 }
